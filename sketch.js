488343ad8f114232a9201bfb26d8f5c7b3dc6897
gfbangie <goolam.fareed123@gmail.com>
2020-03-23 19:54:51 +0200

    added files

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function sum(a,b){
+  return a+b;
+}
+exports.sum=sum;
+function sub(a,b){
+  return a-b;
+}
+
+exports.sub=sub;
---
+++ sketch.test.js
@@ -0,0 +1,11 @@
+const {sum,sub}=require('./sketch');
+
+test('add 1+2 to equal 3',()=>{
+  expect(sum(1,2)).toBe(3);
+});
+
+
+
+test('sub 1-1 to equal 0',()=>{
+   expect(sub(1,1)).toBe(0);
+ });
